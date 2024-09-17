2e68e86c226ae15d8c4c4175abd19b4eb3ef6033
Gabriel Bento <gabrielbgwork@gmail.com>
2024-09-16 22:50:08 -0300

    Service layer, component registration

--- src/main/java/com/gabriel/workshop_springboot3_jpa/resources/UserResource.java
+++ src/main/java/com/gabriel/workshop_springboot3_jpa/resources/UserResource.java
@@ -1,20 +1,38 @@
 package com.gabriel.workshop_springboot3_jpa.resources;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabriel.workshop_springboot3_jpa.entities.User;
+import com.gabriel.workshop_springboot3_jpa.services.UserService;
+
+import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "gabriel", "gabriel", "gabriel", "gabriel");
+	public ResponseEntity<List<User>> findAll(){
+		List<User> u = service.findAll();
 		return ResponseEntity.ok().body(u);
 				
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+		
 	}
 }
