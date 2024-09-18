c7c0607168f8d77325cfae1a3bceed82f998d216
Gabriel Bento <gabrielbgwork@gmail.com>
2024-09-18 01:40:19 -0300

    Insert user

--- src/main/java/com/gabriel/workshop_springboot3_jpa/resources/UserResource.java
+++ src/main/java/com/gabriel/workshop_springboot3_jpa/resources/UserResource.java
@@ -1,18 +1,26 @@
 package com.gabriel.workshop_springboot3_jpa.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabriel.workshop_springboot3_jpa.entities.User;
 import com.gabriel.workshop_springboot3_jpa.services.UserService;
 
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletSecurityElement;
 import jakarta.websocket.server.PathParam;
 
 @RestController
@@ -34,5 +42,12 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> saveUser(@RequestBody User user) {
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
 	}
 }
