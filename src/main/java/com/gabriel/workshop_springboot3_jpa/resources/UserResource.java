3f4d9546773bf97ab1ecf25774a0a9203c336517
Gabriel Bento <gabrielbgwork@gmail.com>
2024-09-18 01:59:12 -0300

    Update user

--- src/main/java/com/gabriel/workshop_springboot3_jpa/resources/UserResource.java
+++ src/main/java/com/gabriel/workshop_springboot3_jpa/resources/UserResource.java
@@ -2,16 +2,14 @@ package com.gabriel.workshop_springboot3_jpa.resources;
 
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +17,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabriel.workshop_springboot3_jpa.entities.User;
 import com.gabriel.workshop_springboot3_jpa.services.UserService;
-
-import jakarta.servlet.Servlet;
-import jakarta.servlet.ServletSecurityElement;
-import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -57,4 +51,12 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user){
+		user = service.update(id, user);
+		return ResponseEntity.ok().body(user);
+	}
+	
+	
 }
