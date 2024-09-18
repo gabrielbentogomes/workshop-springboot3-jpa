df5ddf118ef7139b426a90e38e191f4e3e002a45
Gabriel Bento <gabrielbgwork@gmail.com>
2024-09-18 01:17:04 -0300

    Payment, one-to-one association

--- src/main/java/com/gabriel/workshop_springboot3_jpa/entities/Order.java
+++ src/main/java/com/gabriel/workshop_springboot3_jpa/entities/Order.java
@@ -10,13 +10,16 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gabriel.workshop_springboot3_jpa.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -39,6 +42,9 @@ public class Order implements Serializable{
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 		
@@ -86,11 +92,19 @@ public class Order implements Serializable{
 			this.orderStatus = orderStatus.getCode();
 		}
 	}
-
+	
 	public Set<OrderItem> getItems() {
 		return items;
 	}
 
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
 
 	@Override
 	public int hashCode() {
